574c65169f6b307ddd8e603610b15c997da2ede8
Trevor Smith <trevorsm@pm.me>
2022-10-31 13:08:31 -0600

    fixed login redirect

--- client/src/auth/ProtectedRoute.tsx
+++ client/src/auth/ProtectedRoute.tsx
@@ -1,13 +1,25 @@
-import { Navigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { useMyInfoQuery } from "../generated/graphql"
 
 export function ProtectedRoute({ children }: { children: JSX.Element }) {
   const { data, loading } = useMyInfoQuery()
+  const navigate = useNavigate()
 
   const path = window.location.pathname
 
   if (loading) return <div>Loading...</div>
-  if (!data) return <Navigate to={"/login" + "?redirect=" + path} />
+  if (data) return <div className="App">{children}</div>
 
-  return <div className="App">{children}</div>
+  return (
+    <div>
+      <p>Please login to access {path}</p>
+      <button
+        onClick={() => {
+          navigate("/login" + "?redirect=" + path)
+        }}
+      >
+        login
+      </button>
+    </div>
+  )
 }
