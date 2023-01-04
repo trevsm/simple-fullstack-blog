9dcd8a88fb31981e926fcc9830d2dc910349b995
Trevor Smith <trevorsm@pm.me>
2023-01-04 14:12:56 -0700

    fixed introspection script

--- client/scripts/introspect.sh
+++ client/scripts/introspect.sh
@@ -3,4 +3,7 @@
 # Source the environment variables
 set -a; source ../.env; set +a
 
-npx rover graph introspect http://localhost:5000/graphql > src/api/generated/schema.graphql
+# Introspect the GraphQL schema
+# curl -sSL https://rover.apollo.dev/nix/latest | sh
+
+rover graph introspect http://localhost:5000/graphql > src/api/generated/schema.graphql
